e3799fc40d064a8a32e9ddd7124e552fb0b75bf7
Hossein Keshavarz <hoseinkeshavarz1997@gmail.com>
2019-08-05 15:47:45 +0430

    4

---
+++ test/SimpleTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SimpleTest {
+    @Test
+    public void test() {
+        assertEquals(24, 4 * 6);
+    }
+}
